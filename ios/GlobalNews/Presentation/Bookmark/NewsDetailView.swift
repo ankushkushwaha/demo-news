4a436a3efae3f43f10fcd356822d50b7cde98bc2
Ankush Kushwaha <ankush7377@gmail.com>
2026-06-08 22:06:00 +0300

    Remove back button from news detail view

--- ios/GlobalNews/Presentation/Bookmark/NewsDetailView.swift
+++ ios/GlobalNews/Presentation/Bookmark/NewsDetailView.swift
@@ -2,11 +2,15 @@ import SwiftUI
 
 struct NewsDetailView: View {
     let urlString: String
-    
+    @Environment(\.dismiss) private var dismiss
+
     var body: some View {
         if let url = URL(string: urlString) {
-            SafariView(url: url)
-                .ignoresSafeArea()
+            SafariView(url: url) {
+                dismiss()
+            }
+            .navigationBarBackButtonHidden(true)
+            .ignoresSafeArea()
         } else {
             ContentUnavailableView(
                 "Invalid URL",
